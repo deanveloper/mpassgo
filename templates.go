5b420cd7402d9a25ecf31bbe37219d9f0fe4eb80
Dean Bassett <dean@deanveloper.com>
2017-11-02 17:19:23 -0400

    formatted

--- templates.go
+++ templates.go
@@ -2,62 +2,62 @@ package mpassgo
 
 type TemplateSet []string
 
-var Maximum = TemplateSet {
-    "anoxxxxxxxxxxxxxxxxx",
-    "axxxxxxxxxxxxxxxxxno",
+var Maximum = TemplateSet{
+	"anoxxxxxxxxxxxxxxxxx",
+	"axxxxxxxxxxxxxxxxxno",
 }
 
-var Long = TemplateSet {
-    "CvcvnoCvcvCvcv",
-    "CvcvCvcvnoCvcv",
-    "CvcvCvcvCvcvno",
-    "CvccnoCvcvCvcv",
-    "CvccCvcvnoCvcv",
-    "CvccCvcvCvcvno",
-    "CvcvnoCvccCvcv",
-    "CvcvCvccnoCvcv",
-    "CvcvCvccCvcvno",
-    "CvcvnoCvcvCvcc",
-    "CvcvCvcvnoCvcc",
-    "CvcvCvcvCvccno",
-    "CvccnoCvccCvcv",
-    "CvccCvccnoCvcv",
-    "CvccCvccCvcvno",
-    "CvcvnoCvccCvcc",
-    "CvcvCvccnoCvcc",
-    "CvcvCvccCvccno",
-    "CvccnoCvcvCvcc",
-    "CvccCvcvnoCvcc",
-    "CvccCvcvCvccno",
+var Long = TemplateSet{
+	"CvcvnoCvcvCvcv",
+	"CvcvCvcvnoCvcv",
+	"CvcvCvcvCvcvno",
+	"CvccnoCvcvCvcv",
+	"CvccCvcvnoCvcv",
+	"CvccCvcvCvcvno",
+	"CvcvnoCvccCvcv",
+	"CvcvCvccnoCvcv",
+	"CvcvCvccCvcvno",
+	"CvcvnoCvcvCvcc",
+	"CvcvCvcvnoCvcc",
+	"CvcvCvcvCvccno",
+	"CvccnoCvccCvcv",
+	"CvccCvccnoCvcv",
+	"CvccCvccCvcvno",
+	"CvcvnoCvccCvcc",
+	"CvcvCvccnoCvcc",
+	"CvcvCvccCvccno",
+	"CvccnoCvcvCvcc",
+	"CvccCvcvnoCvcc",
+	"CvccCvcvCvccno",
 }
 
-var Medium = TemplateSet {
-    "CvcnoCvc",
-    "CvcCvcno",
+var Medium = TemplateSet{
+	"CvcnoCvc",
+	"CvcCvcno",
 }
 
-var Short = TemplateSet {
-    "Cvcn",
+var Short = TemplateSet{
+	"Cvcn",
 }
 
-var Basic = TemplateSet {
-    "aaanaaan",
-    "aannaaan",
-    "aaannaaa",
+var Basic = TemplateSet{
+	"aaanaaan",
+	"aannaaan",
+	"aaannaaa",
 }
 
-var Pin = TemplateSet {
-    "nnnn",
+var Pin = TemplateSet{
+	"nnnn",
 }
 
-var charTemplates = map[rune] string {
-    'V': "AEIOU",
-    'C': "BCDFGHJKLMNPQRSTVWXYZ",
-    'v': "aeiou",
-    'c': "bcdfghjklmnpqrstvwxyz",
-    'A': "AEIOUBCDFGHJKLMNPQRSTVWXYZ",
-    'a': "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz",
-    'n': "0123456789",
-    'o': "@&%?,=[]_:-+*$#!'^~;()/.",
-    'X': "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz0123456789!@#$%^&*()",
+var charTemplates = map[rune]string{
+	'V': "AEIOU",
+	'C': "BCDFGHJKLMNPQRSTVWXYZ",
+	'v': "aeiou",
+	'c': "bcdfghjklmnpqrstvwxyz",
+	'A': "AEIOUBCDFGHJKLMNPQRSTVWXYZ",
+	'a': "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz",
+	'n': "0123456789",
+	'o': "@&%?,=[]_:-+*$#!'^~;()/.",
+	'X': "AEIOUaeiouBCDFGHJKLMNPQRSTVWXYZbcdfghjklmnpqrstvwxyz0123456789!@#$%^&*()",
 }
