dce84bfff24a04b70b465f10be9817cba46031c1
deanveloper <dean@deanveloper.com>
2018-05-27 19:08:14 -0400

    simplify .gitignore, put binary files in bin folder

--- build/build.fish
+++ build/build.fish
@@ -2,11 +2,10 @@
 
 
 # remove old files
-for file in (ls)
-	if [ $file != 'build.fish' ]
-		rm $file
-	end
+if [ -e bin ]
+	rm -rf bin
 end
+mkdir bin
 
 # set some variables detailing GOOS/GOARCH targets
 set oslist linux darwin windows
@@ -16,12 +15,12 @@ set mpw_path github.com/deanveloper/mpassgo/cli/mpw
 # build to all the compile targets
 for os in $oslist
 	for ar in $archlist
-		env GOOS=$os GOARCH=$ar go build -o mpw-"$os"_"$ar" $mpw_path
+		env GOOS=$os GOARCH=$ar go build -o bin/mpw-"$os"_"$ar" $mpw_path
 	end
 end
 
 # move to more human-friendly names
-for file in (ls mpw*)
+for file in (ls bin)
 	set new_name $file
 	set new_name (string replace 'darwin' 'mac' $new_name)
 	set new_name (string replace '386' '32' $new_name)
@@ -32,7 +31,7 @@ for file in (ls mpw*)
 	end
 	
 	if [ $file != $new_name ]	
-		mv $file $new_name
+		mv bin/$file bin/$new_name
 	end
 end
 	
