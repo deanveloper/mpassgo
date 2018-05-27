0d37f5ec39e0c7dd370c2ed5d5a79c780e694ba5
deanveloper <dean@deanveloper.com>
2018-05-27 18:48:27 -0400

    Added build files

---
+++ build/build.fish
@@ -0,0 +1,38 @@
+#!/usr/bin/env fish
+
+
+# remove old files
+for file in (ls)
+	if [ $file != 'build.fish' ]
+		rm $file
+	end
+end
+
+# set some variables detailing GOOS/GOARCH targets
+set oslist linux darwin windows
+set archlist 386 amd64
+set mpw_path github.com/deanveloper/mpassgo/cli/mpw
+
+# build to all the compile targets
+for os in $oslist
+	for ar in $archlist
+		env GOOS=$os GOARCH=$ar go build -o mpw-"$os"_"$ar" $mpw_path
+	end
+end
+
+# move to more human-friendly names
+for file in (ls mpw*)
+	set new_name (string replace 'darwin' 'mac' $file)
+	set new_name (string replace '386' '32' $file)
+	set new_name (string replace 'amd64' '64' $file)
+	switch $file
+	case "*windows*"
+		set new_name "$new_name".exe
+	end
+	
+	if [ $file != $new_name ]	
+		mv $file $new_name
+	end
+end
+	
+
